8e387d52285342b8bb52ac48ee5fbbad45774990
hejie <hejie@zhixueyun.com>
2020-12-31 14:09:08 +0800

    release test

--- app/src/main/java/com/wangzuo/bintarydemo/MainActivity.java
+++ app/src/main/java/com/wangzuo/bintarydemo/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.wangzuo.testmodule.DemoUtils;
+
+import java.nio.file.DirectoryNotEmptyException;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DemoUtils.getValue();
     }
 }
---
+++ testmodule/src/main/java/com/wangzuo/testmodule/DemoUtils.java
@@ -0,0 +1,9 @@
+package com.wangzuo.testmodule;
+
+public class DemoUtils {
+
+    public static String getValue(){
+        return "hello";
+    }
+
+}
